1c929da5131a3a50283473ed9f31f97da4a9e2e8
mr7aali <mr7aali@gmail.com>
2022-09-05 00:26:46 +0600

    copy pest from another file which was git-Nothin file

---
+++ Js/java.js
@@ -0,0 +1,93 @@
+const loadCatagoris =()=>{
+    const url = `https://openapi.programming-hero.com/api/news/categories`;
+    fetch(url)
+    .then(res => res.json())
+    // .then(data => displayCatagoris(data.data))
+    .then(data => displayCatagoris(data.data.news_category))
+}
+
+const displayCatagoris=(Catagoris)=>{
+    const catagoryNav = document.getElementById('Catagory');
+
+    for(const Catagory of Catagoris){
+        //  console.log(Catagory);
+
+      const crteatSpan =document.createElement('span');
+      crteatSpan.innerHTML =`
+      <span onclick="getIdForSpanTag('${Catagory.category_id}')">${Catagory.category_name} </span>   
+      `;
+      catagoryNav.appendChild(crteatSpan);
+
+      
+    }
+}
+
+const getIdForSpanTag=(id)=>{
+//console.log(id);
+    const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
+    fetch(url)
+    .then(res=>res.json())
+    .then(  data =>displayNews(data.data));
+}
+
+const displayNews = (newses)=>{
+    //console.log(newses);
+  const newsContainer =document.getElementById('full-news-container');
+  newsContainer.innerHTML=``;
+    for(const news of newses){
+        console.log(news.total_view);
+        
+        const newDiv = document.createElement('div');
+
+  if(news.details.length > 100){
+    news.details =  (news.details.slice(0,500))  ;
+    //console.log(sliceNews)
+  }
+  else{
+     sliceNews = news.details;
+  }
+   const date = news.author.published_date.slice(0,10);
+
+        newDiv.innerHTML =`
+        <article>
+        <div class="img-container">
+             <img src="${news.thumbnail_url}" alt="">
+        </div>
+        <div class="news-blog">
+             <div>
+               <h1>${news.title}</h1>
+               <p>${news.details} ....</p>
+             </div>
+             <div class="author-details"> 
+                        <div class="my-div">
+                              <div class="author-img">
+                                 <img src="${news.author.img}" alt="">
+                                
+                              </div>
+                              <div class="author-name">
+                               <p> ${news.author.name}</p>
+                               <p>${date}</p>
+                              </div>
+                        </div>
+                        <div><span><i class="fa-solid fa-eye"></i> ${news.total_view}k</span></div>
+                        <div id="Rating"> 
+                         <i class="fa-solid fa-star"></i>
+                         <i class="fa-solid fa-star"></i>
+                         <i class="fa-solid fa-star"></i>
+                         <i class="fa-solid fa-star"></i>
+                         <i class="fa-solid fa-star-half-stroke"></i>
+                    
+                       </div>
+                        <div><i class="fa-solid fa-arrow-right"></i></div>
+             </div>
+        </div>
+  </article>
+        `;
+  
+        newsContainer.appendChild(newDiv);
+    }
+}
+
+
+
+loadCatagoris();
